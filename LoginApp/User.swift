42e89853734287d7a5bfe1eeb81cfbd8d856b570
DenysIlnytskyi <380984511271k@gmail.com>
2022-08-07 16:40:16 +0200

    Adding model

---
+++ LoginApp/User.swift
@@ -0,0 +1,38 @@
+//
+//  User.swift
+//  LoginApp
+//
+//  Created by Денис Ільницький on 07/08/2022.
+//
+
+struct User {
+    let login: String
+    let password: String
+    let person: Person
+    
+    static func getUser() -> User {
+        User(
+            login: "login",
+            password: "password",
+            person: Person.getPerson()
+        )
+    }
+}
+
+struct Person {
+    let name: String
+    let surname: String
+    let image: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getPerson() -> Person {
+        Person(
+            name: "Denys",
+            surname: "Ilnytskyi",
+            image: "TestImage"
+        )
+    }
+}
